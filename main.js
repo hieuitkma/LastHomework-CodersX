b7832c7f15d5c480997af1fec4e6c7b71f17fa90
hieuitkma <hieuitkma@gmail.com>
2020-05-03 12:39:29 +0700

    Add 6, 7, modify 5

--- main.js
+++ main.js
@@ -55,12 +55,48 @@ function findMax(a, b, c) {
 // viết hàm kiểm tra xem một chuỗi bắt đầu bằng "Java" hay không 
 function startWith(str) {
     // viết code ở đây.
-    let tach = str.split('');
-    console.log(tach);
-    for (var i = 0; tach.length; i++) {
-        if (tach[0] == 'J' && tach[1] == 'a' && tach[2] == 'v' && tach[3] == 'a') {
-            return true;
-        }
-        return false;
+    // startsWidth : kiem tra xem chuoi bat dau duoc chi dinh
+    console.log(str.startsWith('Java'));
+}
+startWith('Javascript');
+
+// ---------------------------- bai 6
+/*
+  - Viết hàm findMaxDiff nhận tham số là một mảng integer (mảng số nguyên)
+  - Trả về sự khác biệt lớn nhất giữa hai phần tử liền kề của mảng đó.
+  - Nếu mảng có 1 phần tử hoặc không có phần tử nào trả về 0
+  Example: 
+  [1, -10, 5, 18, -9, 5] => 27
+*/
+function findMaxDiff(arr) {
+    let maxDiff = 0;
+    for (let i = 0; i < arr.length; i++) {
+        //Math.abs trar ve gia tri tuyet doi
+        maxDiff = (maxDiff < Math.abs(arr[i] - arr[i + 1])) ? Math.abs(arr[i] - arr[i + 1]) : maxDiff;
     }
+    return maxDiff;
+}
+
+// -----------------------------bai 7
+//Viết 1 function kiểm tra số lương kí tự 'p' và 't' của 1 chuỗi có bằng nhau hay không
+//=============================
+//input : string
+//output : true or false
+//=============================
+
+function equal_pt(str) {
+    // viết code ở đây.
+    let countP = 0;
+    let countT = 0;
+    for (let i = 0; i < str.length; i++) {
+        //charAt(): Gia tri cua vi tri can xet
+        if (str.charAt(i) == 'p') {
+            countP++;
+        }
+        if (str.charAt(i) == 't') {
+            countT++;
+        }
+    }
+    if (countT == countP) return true;
+    return false;
 }
